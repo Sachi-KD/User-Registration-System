96c7df358f56ac8a4009a2212fcbbd8678c9eec1
Sachi <sachithra@efuturesworld.com>
2024-04-29 17:21:47 +0530

    first commit

---
+++ MYAPP/resources/views/welcome.blade.php
@@ -0,0 +1,58 @@
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <!-- Styles -->
+        <style>
+    body {
+      font-family: sans-serif;
+      background-image: url('https://wallpaperaccess.com/full/5137774.jpg'); /* Replace 'your-background-image.jpg' with your image file */
+      background-size: cover; 
+      background-position: center; 
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+     
+    }
+    .btn-container {
+      margin-top:2px; 
+      margin-bottom: 160px;
+    }
+   
+    .custom-btn {
+      width: 300px; 
+      height: 50px;
+      background-color: black; 
+      border-color: black;
+      display: flex; /* Use flexbox for centering */
+      justify-content: center; /* Center horizontally */
+      align-items: center; /* Center vertically */
+      text-align: center; /* Align text to center */
+    }
+    .custom-btn:hover {
+     background-color: #8c8c8c; /* Darken button color on hover */
+      border-color: #8c8c8c; /* Darken button border color on hover */
+    }
+    h2 {
+      margin-top: 50px; 
+      margin-bottom: 130px;
+    }
+  </style>
+    </head>
+    <body>
+        <h2>User's Registration System</h2>
+        <div class="btn-container">
+        <a href="/admin" class="btn btn-primary custom-btn">View Users </a><br>
+        </div>
+    </body>
+</html>
